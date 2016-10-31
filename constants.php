b49291cdf83e29dd9058d536ca48fe0f62e9b765
Chris Uberti <cuberti87@gmail.com>
2016-10-31 22:01:43 +0000

    Ok, so lots of work to do here.
    Might need to set up the constants file as a class, I'm having issues with using global variables within functions.
    
    Temporary workaround is just using the global function within the calculation functions (this isn't great practice).
    Might just go with this for the time being to get things working.

--- constants.php
+++ constants.php
@@ -1,19 +1,19 @@
 <?php
-
+class Constants{
 
 //List of constants that can be used throughout the program
-
 $grav = 9.8067; //m/s
 $MMair = 28.965; //g-mol-1
 $R_air = 287.06; //J-kg-1 K-1
 $TempLapse = 0.0065; //K/m
-$T_sl = 288.15; //Sea Level standard temp Kelvin
+$T_sl = 288.15;
 $P_sl = 101.33; //Sea Level standard pressure kPa
 
 
 
+
 //convert meters to feet;
-function meters2feet($m){return 3.2808*$m;}
+function feet2meters($ft){return 3.2808*$ft;}
 
 //convert celcius to farienheight
 function celcius2farienheight($cel){return 1.8*$cel+32;}
@@ -25,17 +25,21 @@ function Kelvin($c){return $c+273.15;}
 function TotalMass($rider, $bike){return $bike+$rider;}
 
 function densityCalc($Tair, $humidity, $alt){
+	global $grav, $MMair, $R_air, $TempLapse, $T_sl, $P_sl;
 	$t_rank = fariengheight2kelvin($Tair);
-	$P_sat = pow(M_E, ((77.345+0.0057*$t_rank- 7235/$t_rank))/$t_rank^8.2;
-
+	$P_sat = pow(M_E, (77.345+0.0057*$t_rank-7235/$t_rank))/pow($t_rank,8.2);
+	$P_dry =$P_sl*pow((1-($TempLapse* feet2meters($alt))/288.15), ($grav/($R_air*$TempLapse)));
+	$rho_dry = (($P_dry*1000)/$R_air*$t_rank);
 	//Density of water vapor in air
 	$rho_wet = (0.0022*$P_sat/$P_dry) * ($humidity/100);
 	$x = $rho_wet/$rho_dry; 
-	$rho_actual = $rho_dry *((1+$x)/(1+1.609*$x)); //This is some empherical ratio to determine actual density based on humidity ratio
+	$rho_actual = $rho_dry *((1+$x)/(1+1.609*$x));//This is some empherical ratio to determine actual density based on humidity ratio
+
+	return $rho_actual;
 }
 
 
 
-
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,31 +1,44 @@
 <?php 
 
 require('constants.php');
+if(!empty($_POST['submit'])){
+	
+	echo "Temperature: " . fariengheight2kelvin($_POST['TempAir']);
+	echo "Pressure: ". densityCalc($_POST['TempAir'], $_POST['RelHumidity'], $_POST['alt']);
+}
 
-echo "Temperature: " . celcius2farienheight($_POST['temp'])
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<title>Wheel Selector</title>
 </head>
+
 <body>
 	<div>
-		<form action="index.php" method = "POST">
+		<form action="index.php" method="POST">
 			<table>
-			<tr>
-				<input type="number" name ="TempAir" value = 70> Degrees F
+				<tr>
+				<td>Air Temperature: </td><td><input type="number" name="TempAir" value=7 0> Degrees F</td>
 				</tr>
 				<tr>
-				<input type="number" name ="RelHumidity" value=10> %
+					<td>Relative Humidity: </td><td><input type="number" name="RelHumidity" value=10> %</td>
 				</tr>
-				
-				<input type="submit">
+				<tr>
+					<td>Altitude: </td><td><input type="number" name="alt" value=0> feet</td>
+				</tr>
+				<tr>
+					
+					<td><input type="submit" name="submit"></td>
+				</tr>
 			</table>
 		</form>
 	</div>
-	
+
 </body>
+
 </html>
