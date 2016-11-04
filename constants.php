fcc5a61b8a55a5cd258906068d69a06f178f55bf
Chris Uberti <cuberti87@gmail.com>
2016-11-04 11:29:22 +0000

    Updates to the density calculation. Still have not resolved the corret formula for figuring out how to density

--- constants.php
+++ constants.php
@@ -15,6 +15,9 @@ $P_sl = 101.33; //Sea Level standard pressure kPa
 //convert meters to feet;
 function feet2meters($ft){return $ft*0.3048;}
 
+//
+function inHg2kPa($p){return $p*3.38639;}
+
 //convert celcius to farienheight
 function celcius2farienheight($cel){return 1.8*$cel+32;}
 function fariengheight2kelvin($t){return Rankine($t)*0.555556;}
@@ -24,23 +27,84 @@ function Kelvin($c){return $c+273.15;}
 
 function TotalMass($rider, $bike){return $bike+$rider;}
 
-function densityCalc($Tair, $humidity, $alt){
+function densityCalc($Tair, $humidity, $alt, $P_a){
 	global $grav, $MMair, $R_air, $TempLapse, $T_sl, $P_sl;
+	//convert units:
 	$t_rank = fariengheight2kelvin($Tair);
-	//Saturation pressure of wator vapor in air
-	$P_sat = exp((77.345+0.0057*$t_rank-7235/$t_rank))/pow($t_rank,8.2);
-	//Density of water in air (with humidity factored in)
-	$rho_wet = (0.0022*$P_sat/$t_rank)*$humidity;
-	//Calculate Pressure of dry air only (no dilution of pressure due to water vapor in ari)
-	$P_dry =$P_sl*pow((1-($TempLapse* feet2meters($alt))/$T_sl), ($grav/($R_air*$TempLapse)));
-	$rho_dry = (($P_dry*1000)/($R_air*$t_rank));
-	//Density of water vapor in air
-	$x = $rho_wet/$rho_dry; 
+	$P_a = inHg2kPa($P_a);
 	
-	//Full actual density of air with water vapor
-	$rho_actual = $rho_dry *((1+$x)/(1+1.609*$x));//This is some empherical ratio to determine actual density based on humidity ratio
+	
+	//Saturation pressure of wator vapor in air 
+	////THIS IS GOOD
+	$P_sat = exp((77.345+0.0057*$t_rank-7235/$t_rank))/pow($t_rank,8.2)/1000;
+	
+	///NOT RIGHT
+	//$P_vap = ($humidity)/$P_sat;
 
-	return $rho_actual;
+	//This only takes into account altitude gain!!!!
+	$P_dry =$P_sl*pow((1-($TempLapse* feet2meters($alt))/$T_sl), ($grav/($R_air*$TempLapse)));
+	//Dalton's Law:
+	$P_vap = $P_a-$P_dry;
+	
+	//THIS IS CORRECT (IDEAL GAS LAW)
+	$rho_dry = (($P_dry*1000)/($R_air*$t_rank));
+	
+	//THIS IS ALSO CORRECT
+	$x = 0.62198*$P_vap / ($P_a-$P_vap);
+	
+
+	
+	
+	////Density of water in air (with humidity factored in)
+	//if($humidity<1){$fact=$humidity;}else{$fact=1;}//some built in logic to make sure you're using humidity and not pressure
+	//$rho_wet = (0.0022*$P_sat/$t_rank)*$fact;
+	////Calculate Pressure of dry air only (no dilution of pressure due to water vapor in ari)
+	//
+	//
+	//
+	//
+	//$rho_dry = (($P_dry*1000)/($R_air*$t_rank));
+	////Density of water vapor in air
+	//if($humidity > 1){
+	//	$x = 0.62198*($P_sat/1000)/($P_a - $P_sat);
+	//	echo $P_sat/1000;
+	//}else{
+	//	$x = $rho_wet/$rho_dry; 
+	//}
+	//
+	////Full actual density of air with water vapor
+	$rho_actual = $rho_dry *((1+$x)/(1+1.609*$x));//This is some empherical ratio to determine actual density based on humidity ratio
+	
+	//populate output table:
+	$data['P_a']=$P_a;
+	$data['P_sat']=$P_sat;
+	$data['P_dry']=$P_dry;
+	$data['P_vap']=$P_vap;
+	$data['x']=$x;
+	$data['rho_dry']=$rho_dry;
+	$data['rho_actual']=$rho_actual;
+	
+	
+	return $data;
+}
+
+function densityCalc($Tair, $humidity, $alt, $P_a){
+	global $grav, $MMair, $R_air, $TempLapse, $T_sl, $P_sl;
+	//convert units:
+	$t_rank = fariengheight2kelvin($Tair);
+	$P_a = inHg2kPa($P_a);
+
+	//populate output table:
+	$data['P_a']=$P_a;
+	$data['P_sat']=$P_sat;
+	$data['P_dry']=$P_dry;
+	$data['P_vap']=$P_vap;
+	$data['x']=$x;
+	$data['rho_dry']=$rho_dry;
+	$data['rho_actual']=$rho_actual;
+	
+	
+	return $data;
 }
 
 
--- index.php
+++ index.php
@@ -3,8 +3,16 @@
 require('constants.php');
 if(!empty($_POST['submit'])){
 	
-	echo "Temperature: " . fariengheight2kelvin($_POST['TempAir']);
-	echo "Density: ". densityCalc($_POST['TempAir'], $_POST['RelHumidity'], $_POST['alt']);
+	//$rho = densityCalc($_POST['TempAir'], $_POST['RelHumidity'], $_POST['alt']);
+	//echo "<br>Temperature: " . fariengheight2kelvin($_POST['TempAir']);
+	$data = densityCalc($_POST['TempAir'], $_POST['RelHumidity'], $_POST['alt'], $_POST['P_a']);
+	foreach ($data as $key => $value) {
+		echo "<br>" . $key . ": " . $value;
+	}
+	
+	
+	
+	
 }
 
 
@@ -19,32 +27,64 @@ if(!empty($_POST['submit'])){
 </head>
 
 <body>
-	<div>
+	<div id = "input-form">
 		<form action="index.php" method="POST">
 			<?php
 			if(!empty($_POST['submit'])){
-				$Tair = $_POST['TempAir'];$humidity = $_POST['RelHumidity'];$alt = $_POST['alt'];
+				$Tair = $_POST['TempAir'];$humidity = $_POST['RelHumidity'];$alt = $_POST['alt'];$P_a = $_POST['P_a'];
 			}else{
 				$Tair = 50;$humidity = 10;$alt = 100;
-			}
-			?>
-			<table>
-				<tr>
-				<td>Air Temperature: </td><td><input type="number" name="TempAir" value= <?php echo $Tair;?>> Degrees F</td>
-				</tr>
-				<tr>
-					<td>Relative Humidity: </td><td><input type="number" name="RelHumidity" value=<?php echo $humidity;?>> %</td>
-				</tr>
-				<tr>
-					<td>Altitude: </td><td><input type="number" name="alt" value=<?php echo $alt;?>> feet</td>
-				</tr>
-				<tr>
-					
-					<td><input type="submit" name="submit"></td>
-				</tr>
-			</table>
+			}?>
+				<table>
+					<tr>
+					<td><h4>Weather Metrics</h4></td>
+					</tr>
+					<tr>
+						<td>Air Temperature: </td>
+						<td><input type="number" name="TempAir" value=<?php echo $Tair;?>> Degrees F</td>
+					</tr>
+					<tr>
+						<td>Relative Humidity: </td>
+						<td><input type="text" name="RelHumidity" value=<?php echo $humidity;?>> %</td>
+					</tr>
+					<tr>
+						<td>Air Pressure: </td>
+						<td><input type="text" name="P_a" value=<?php echo $P_a;?>> inHg</td>
+					</tr>
+					<tr>
+						<td>Altitude: </td>
+						<td><input type="number" name="alt" value=<?php echo $alt;?>> feet</td>
+					</tr>
+					<tr>
+						<td>
+							<h4>Rider Metrics</h4>
+						</td>
+					</tr>
+					<tr>
+						<td>Rider Weight: </td>
+						<td><input type="number" name="weight_rider" value=<?php echo $weight_rider;?>> Kilograms</td>
+					</tr>
+					<tr>
+						<td>Bike Weight: </td>
+						<td><input type="number" name="weight_bike" value=<?php echo $weight_bike;?>> Kilograms</td>
+					</tr>
+					<tr>
+						<td>Riding Position: </td>
+						<td>
+							<select name="position" id="">
+								<option value="hoods">Hoods</option>
+								<option value="drops">Drops</option>
+								<option value="super-tuck">SuperTuck</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td><input type="submit" name="submit"></td>
+					</tr>
+				</table>
 		</form>
 	</div>
+	<hr>
 
 </body>
 
