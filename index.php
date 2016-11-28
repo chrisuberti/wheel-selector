0a92a80d4be206d4e35035ad679b0fcdf4b9afba
Chris Uberti <cuberti87@gmail.com>
2016-11-28 07:48:02 -0500

    test

--- index.php
+++ index.php
@@ -3,103 +3,82 @@
 require('constants.php');
 if(!empty($_POST['submit'])){
 	
-	//$rho = densityCalc($_POST['TempAir'], $_POST['RelHumidity'], $_POST['alt']);
-	//echo "<br>Temperature: " . fariengheight2kelvin($_POST['TempAir']);
-	$data = densityCalc($_POST['TempAir'], $_POST['RelHumidity'], $_POST['alt']);
+	//$rho = densityCalc($_POST['Tair'], $_POST['humidity'], $_POST['alt']);
+	//echo "<br>Temperature: " . fariengheight2kelvin($_POST['Tair']);
+	$data = densityCalc($_POST['Tair'], $_POST['humidity'], $_POST['alt']);
 	foreach ($data as $key => $value) {
 		echo "<br>" . $key . ": " . $value;
 	}
 	
-	
-	
-	
 }
 
+include_once('header.php');	
 
+
+
+//Prepare some data
+if(!empty($_POST['submit'])){
+	$Tair = $_POST['Tair'];$humidity = $_POST['humidity'];$alt = $_POST['alt'];$P_a = $_POST['P_a'];
+}else{
+	$Tair = 50;$humidity = 10;$alt = 100; $P_a = 30.00;
+}
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<script type="text/javascript">
+	function density_calc(){
+		var Tair = document.getElementById('Tair').value;
+		var humidity = document.getElementById('humidity').value;
+		var alt = document.getElementById('alt').value;
 
-<head>
-	<meta charset="UTF-8">
-	<title>Wheel Selector</title>
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		var httpreq = new XMLHttpRequest();
+		httpreq.onreas
+           
+	}
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+</script>
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-
-</head>
 
 <body>
-	<div class="jumbotron"><h1>Wheel Selector</h1></div>
-	<div id = "input-form">
-		<form action="index.php" method="POST">
-			<?php
-			if(!empty($_POST['submit'])){
-				$Tair = $_POST['TempAir'];$humidity = $_POST['RelHumidity'];$alt = $_POST['alt'];$P_a = $_POST['P_a'];
-			}else{
-				$Tair = 50;$humidity = 10;$alt = 100; $P_a = 30.00;
-			}?>
-				<table class = "table">
-					<tr>
-					<td><h4>Weather Metrics</h4></td>
-					</tr>
-					<tr>
-						<td><label for="TempAir">Air Temperature: </label></td>
-						<td><input type="number" name="TempAir" value=<?php echo $Tair;?>> Degrees F</td>
-					</tr>
-					<tr>
-						<td>Relative Humidity: </td>
-						<td><input type="text" name="RelHumidity" value=<?php echo $humidity;?>> %</td>
-					</tr>
-					<tr>
-						<td>Altitude: </td>
-						<td><input type="number" name="alt" value=<?php echo $alt;?>> feet</td>
-					</tr>
-					<tr>
-						<td>
-							<h4>Rider Metrics</h4>
-						</td>
-					</tr>
-					<tr>
-						<td>Rider Weight: </td>
-						<td><input type="number" name="weight_rider" value=<?php echo $weight_rider;?>> Kilograms</td>
-					</tr>
-					<tr>
-						<td>Bike Weight: </td>
-						<td><input type="number" name="weight_bike" value=<?php echo $weight_bike;?>> Kilograms</td>
-					</tr>
-					<tr>
-						<td>Riding Position: </td>
-						<td>
-							<select name="position" id="">
-								<option value="hoods">Hoods</option>
-								<option value="drops">Drops</option>
-								<option value="super-tuck">SuperTuck</option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td><input type="submit" name="submit"></td>
-					</tr>
-				</table>
-		</form>
-	</div>
-	<hr>
+<div class="jumbotron"><h1 class="text-centered">Wheel Selector</h1></div>
+<form class = "form-horizontal" id = "form-data">
+<h3 class="text-centered col-md-offset-1">Weather Metrics</h3>
+        <div class="form-group">
+            <label for="Tair" class="label-control col-md-1 col-md-offset-1">Air Temp (F):</label>
+            <div class="col-md-4">
+                <input type="number" id = "Tair" class= "form-control" name="Tair"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="humidity" class="label-control col-md-1 col-md-offset-1">Humidity (%):</label>
+            <div class="col-md-4">
+                <input type="number" id = "humidity" class= "form-control" name="humidity"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="alt" class="label-control col-md-1 col-md-offset-1">Altitude (ft):</label>
+            <div class="col-md-4">
+                <input type="number" id = "alt" class= "form-control" name="alt"/>
+            </div>
+        </div>
+        <!--<div class="form-group">
+            <label for="name" class="label-control col-md-1" for = "subject">Subject</label>
+            <div class="col-md-5">
+                <select name="" class="form-control" id = "subject">
+                    <option value="">Select Subject</option>
+                    <option value="Chemestry">Chemestry</option>
+                    <option value="Physics">Physics</option>
+                    <option value="Computers">Computers</option>
+                    <option value="Math">Math</option>
+                </select>
+            </div>-->
+            <div class="form-group">
+                <div class="col-md-5 col-md-offset-1">
+                    <button class = "btn btn-success btn-block" onclick="density_calc();">Add New Record</button>
+                </div>
+            </div>
+        </div>
+    </form>
+
 
 </body>
 
