2acc87a9e25d974ae71bcf5ff2c7e9087c2c876b
Chris Uberti <cuberti87@gmail.com>
2016-12-06 11:56:35 +0000

    stopping point on working equations before implementing CI

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 
 require('constants.php');
+include_once('components.php');
 if(!empty($_POST['submit'])){
 	
 	//$rho = densityCalc($_POST['Tair'], $_POST['humidity'], $_POST['alt']);
@@ -17,11 +18,11 @@ include_once('header.php');
 
 
 //Prepare some data
-if(!empty($_POST['submit'])){
-	$Tair = $_POST['Tair'];$humidity = $_POST['humidity'];$alt = $_POST['alt'];$P_a = $_POST['P_a'];
-}else{
-	$Tair = 50;$humidity = 10;$alt = 100; $P_a = 30.00;
-}
+//if(!empty($_POST['submit'])){
+//	$Tair = $_POST['Tair'];$humidity = $_POST['humidity'];$alt = $_POST['alt'];$P_a = //$_POST['P_a'];
+//}else{
+//	$Tair = 50;$humidity = 10;$alt = 100; $P_a = 30.00;
+//}
 ?>
 
 <script type="text/javascript">
@@ -29,55 +30,106 @@ if(!empty($_POST['submit'])){
 		var Tair = document.getElementById('Tair').value;
 		var humidity = document.getElementById('humidity').value;
 		var alt = document.getElementById('alt').value;
+		
+		var params = "Tair="+Tair+"&humidity="+humidity+"&alt="+alt;
 
-		var httpreq = new XMLHttpRequest();
-		httpreq.onreas
-           
+		var xmlreq = new XMLHttpRequest();
+		
+		xmlreq.open("POST", "test.php", true);
+		xmlreq.onreadystatechange = function(){
+			if(xmlreq.readyState == 4 && xmlreq.status == 200){
+				console.log(xmlreq.response);
+				document.getElementById('test').innerHTML = xmlreq.responseText;
+				document.getElementById('Tair').value = '';
+				
+			}
+		};
+			
+		
+		
+		xmlreq.send(params);
+         
 	}
 
 </script>
-
-
 <body>
-<div class="jumbotron"><h1 class="text-centered">Wheel Selector</h1></div>
-<form class = "form-horizontal" id = "form-data">
-<h3 class="text-centered col-md-offset-1">Weather Metrics</h3>
-        <div class="form-group">
-            <label for="Tair" class="label-control col-md-1 col-md-offset-1">Air Temp (F):</label>
-            <div class="col-md-4">
-                <input type="number" id = "Tair" class= "form-control" name="Tair"/>
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="humidity" class="label-control col-md-1 col-md-offset-1">Humidity (%):</label>
-            <div class="col-md-4">
-                <input type="number" id = "humidity" class= "form-control" name="humidity"/>
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="alt" class="label-control col-md-1 col-md-offset-1">Altitude (ft):</label>
-            <div class="col-md-4">
-                <input type="number" id = "alt" class= "form-control" name="alt"/>
-            </div>
-        </div>
-        <!--<div class="form-group">
-            <label for="name" class="label-control col-md-1" for = "subject">Subject</label>
-            <div class="col-md-5">
-                <select name="" class="form-control" id = "subject">
-                    <option value="">Select Subject</option>
-                    <option value="Chemestry">Chemestry</option>
-                    <option value="Physics">Physics</option>
-                    <option value="Computers">Computers</option>
-                    <option value="Math">Math</option>
-                </select>
-            </div>-->
+    <div class="jumbotron">
+        <h1 class="text-center">Wheel Selector</h1>
+    </div>
+    <h2 id="test"></h2>
+
+    <form class="form-horizontal" id="form-data" action = "index.php">
+        <div class="col-md-6">
+            <h3 class="text-center col-md-offset-1">Weather Metrics</h3>
             <div class="form-group">
-                <div class="col-md-5 col-md-offset-1">
-                    <button class = "btn btn-success btn-block" onclick="density_calc();">Add New Record</button>
+                <label for="Tair" class="label-control col-md-2 col-md-offset-2">Air Temp (F):</label>
+                <div class="col-md-8">
+                    <input type="number" id="Tair" class="form-control" name="Tair" onkeyup="" />
                 </div>
             </div>
-        </div>
+            <div class="form-group">
+                <label for="humidity" class="label-control col-md-2 col-md-offset-2">Humidity (%):</label>
+                <div class="col-md-8">
+                    <input type="number" id="humidity" class="form-control" name="humidity" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="alt" class="label-control col-md-2 col-md-offset-2">Altitude (ft):</label>
+                <div class="col-md-8">
+                    <input type="number" id="alt" class="form-control" name="alt" />
+                </div>
+            </div>
+            </div>
+            <div class="col-md-6">
+            	 <h3 class="text-center col-md-offset-1">Rider Metrics</h3>
+	            <div class="form-group">
+	                <label for="Wrider" class="label-control col-md-2 ">Rider Weight(kg):</label>
+	                <div class="col-md-8">
+	                    <input type="number" id="Wrider" class="form-control" name="Wrider" />
+	                </div>
+	            </div>
+	            
+	            <div class="form-group">
+	                <label for="Wbike" class="label-control col-md-2">Bike Weight(kg):</label>
+	                <div class="col-md-8">
+	                    <input type="number" id="Wbike" class="form-control" name="Wbike" />
+	                </div>
+	            </div>
+	        
+
+            <div class="form-group">
+	            <label for="wheelset" class="label-control col-md-2" >Wheelset:</label>
+	            <div class="col-md-8">
+	                <select name="wheelset" class="form-control" id = "wheelset">
+	                    <option value="">Select Subject</option>
+						<?php 
+	                	foreach($positions as $position){
+	                		echo "<option value='".$position."'>".$position."</option>";
+	                	}?>
+	                </select>
+	            </div>
+	            </div>
+	         <div class="form-group">
+	            <label for="rider_pos" class="label-control col-md-2">Rider Position:</label>
+	            <div class="col-md-8">
+	                <select name="rider_pos" class="form-control" id = "rider_pos">
+	                	<option value="">Select Subject</option>
+	                	<?php 
+	                	foreach($positions as $position){
+	                		echo "<option value='".$position."'>".$position."</option>";
+	                	}?>
+	                </select>
+	            </div>
+	            </div>
+	            </div>
+	       
+                <div class="col-md-4 col-md-offset-4">
+                    <button class="btn btn-success btn-block" onclick="density_calc();">Add New Record</button>
+                </div>
+              
+	           
     </form>
+            
 
 
 </body>
