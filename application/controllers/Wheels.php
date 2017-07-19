327beec2ab16d4ceebf6a76ea15d72d46972f7a9
Chris Uberti <cuberti87@gmail.com>
2017-07-19 01:19:09 +0000

    made it so the weather function can pass data back to wheels function

--- application/controllers/Wheels.php
+++ application/controllers/Wheels.php
@@ -21,12 +21,19 @@ class Wheels extends MY_Controller {
 	 public function index(){
 	 	$data['title']='Wheel Calculation Input';
 	 	
+	 	if($this->form_validation->run()==FALSE){
+	 		//Failure	
+	 	}else{
+	 		//Success
+	 	}
+	 	
 	 	$data['air_temp']=array(
 	 			'name'=>'air_temp',
 	 			'type'=>'number',
 	 			'value'=>70
 	 			
 	 		);
+	 	$this->form_validation->set_rules('air_temp', 'Air Temperature', 'required');
 	 	$data['distance']=array(
 	 			'name'=>'distance',
 	 			'type'=>'number'
@@ -78,6 +85,10 @@ class Wheels extends MY_Controller {
 	 			'name'=>'rider_height',
 	 			'type'=>'number',
 	 		);
+	 		$data['zip_code']=array(
+	 			'name'=>'zip_code',
+	 			'type'=>'number');
+	 		$this->form_validation->set_rules('zip_code', 'Zip Code', 'required|min_length[5]|max_length[5]');
 	 	
 	     $this->load->view('dressings/header');
 	     $this->load->view('wheel_input', $data);
@@ -110,24 +121,38 @@ class Wheels extends MY_Controller {
 		/chris@radical-design.us
 		/
 	 	*/
+	 	if(isset($_POST['weather_submit'])){
+	 		//this indicates that the weather submit button was pressed
+	 		$zip_code=$_POST['zip_code'];
+			$api_key = '3b0387fbc9acfc15';
+			$url = 'http://api.wunderground.com/api/'.$api_key;
+			$url .='/conditions/q/'.$zip_code.'.json';
+			
+		 	$weather = file_get_contents($url);
+		 	$weather = json_decode($weather);
+			preprint($weather);
+			
+			if(isset($weather->response->error->description)){
+					$this->session->set_flashdata('message', 'Zip Code not found');
+					return FALSE;
+			}
+			//preprint($weather);
+			$data['city_name'] = $weather->current_observation->display_location->full;
+		 	$data['wind_degrees'] = $weather->current_observation->wind_degrees;
+		 	$data['wind_speed'] = $weather->current_observation->wind_mph;
+		 	$data['pressure_in'] = $weather->current_observation->pressure_in;
+		 	$data['alt'] = $weather->current_observation->display_location->elevation;
+		 	$data['temp_f']= $weather->current_observation->temp_f;
+		 	$data['relative_humidity']= (int)trim($weather->current_observation->relative_humidity, "%");
+		 	
+		 	
+		 	
+		 	$data['density_data'] = $this->wheelset->density($data['alt'],$data['temp_f'],$data['relative_humidity']);
+			$this->session->set_flashdata('weather_data', $data);
+			redirect('wheels');
+	 	}
 	 	
-		$api_key = '3b0387fbc9acfc15';
-		
-		
-	 	$weather = file_get_contents('http://api.wunderground.com/api/3b0387fbc9acfc15/conditions/q/45214.json');
-		$weather = json_decode($weather);
-		//preprint($weather);
-	 	$data['wind_degrees'] = $weather->current_observation->wind_degrees;
-	 	$data['wind_speed'] = $weather->current_observation->wind_mph;
-	 	$data['pressure_in'] = $weather->current_observation->pressure_in;
-	 	$data['alt'] = $weather->current_observation->display_location->elevation;
-	 	$data['temp_f']= $weather->current_observation->temp_f;
-	 	$data['relative_humidity']= (int)trim($weather->current_observation->relative_humidity, "%");
-	 	
-	 	
-	 	
-	 	$data['density_data'] = $this->wheelset->density($data['alt'],$data['temp_f'],$data['relative_humidity']);
-	 	preprint($data);
+
 	 	return($data);
 	 	//http://api.wunderground.com/api/3b0387fbc9acfc15/conditions/q/CA/San_Francisco.json
 	 }
--- application/views/wheel_input.php
+++ application/views/wheel_input.php
@@ -12,6 +12,7 @@
             <?php 
             if(validation_errors()){echo validation_errors('<p class = "error">','</p>');}?>
             <?php echo  ($this->session->flashdata('message')); ?>
+            <?php preprint($this->session->flashdata('weather_data'));?>
             <?php echo anchor('wheels/get_weather');?>
             <?php echo form_open_multipart('wheels/density');?>
             
@@ -33,7 +34,18 @@
                         <td><?php echo form_label('Humidity (%): ', 'humidity');?></td>
                         <td><?php echo form_input($humidity);?></td>
                     </tr>
-                    
+                    <tr>
+                        <td><h4>Or: </h4></td>
+                        
+                    </tr>
+                    <tr>
+                        <td><?php echo form_label('Enter Zip Code: ', 'zip_code');?></td>
+                        <td><?php echo form_input($zip_code);?></td>
+                    </tr>
+                    <tr>
+                        <td><p><?php echo form_submit(array('name'=>'weather_submit', 'value'=>'Calculate', 'formaction'=>'wheels/get_weather'));?></p></td>
+                    </tr>
+                   
                     <tr><td><h3>Ride Metrics: </h3></td></tr>
                     
                     <tr>
@@ -49,7 +61,7 @@
                         <td><?php echo form_label('Ride Type: ', 'ride_type');?></td>
                         <td><?php echo form_dropdown($ride_type,$ride_type_options);?></td>
                     </tr>
-                   
+
                 </table>
             </div>
             <div class="col-md-4">
@@ -75,11 +87,19 @@
                     <tr>
                         <td><hr></td>
                     </tr>
-                    <tr>
-                        <td><?php echo form_submit('wheel_submit', 'Calculate');?></td>
-                    </tr>
+
                 </table>
             </div>
+            <div class="col-md-3">
+                This is a test
+            </div>
+        </div>
+
+
+    <div class="row">
+                    
+                    <?php echo form_submit(array('name'=>'wheel_submit', 'value'=>'Calculate', 'formaction'=>'wheels/get_weather'));?></td>
+                    
     </div>
 </div>
 
